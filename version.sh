84bedcededee146b84a433bb076c86518241d2bd
Chenxing Li <ChenxingLi@users.noreply.github.com>
2022-06-24 15:38:59 +0800

    Add changes for recent CIPs

--- version.sh
+++ version.sh
@@ -4,14 +4,19 @@ function build() {
     (git diff --quiet && git diff --cached --quiet) || nodiff=false
     if $nodiff; then
         commitid=$(git rev-parse --short HEAD)
-        version="Nightly "$commitid
+        version=$commitid
         timestamp=$(git show -s --format=%ct HEAD)
     else
         version="Temporary"
         timestamp=$(date +%s)
     fi
 
-    date=$(LC_ALL=C date -d '@'$timestamp +"%B %e, %Y")
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        date=$(LC_ALL=C date -r $timestamp +"%B %e, %Y")
+    else
+        date=$(LC_ALL=C date -d '@'$timestamp +"%B %e, %Y")    
+    fi
+    
 
     echo "\\newcommand{\\fdate}{"$date"}" > version.tex
     echo "\\newcommand{\\version}{"$version"}" >> version.tex
